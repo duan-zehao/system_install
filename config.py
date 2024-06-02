cc5c2d9fcfb25d19caf565ec69b49d7cc2f9a466
duanzehao <2523305230@qq.com>
2024-06-02 09:02:57 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+server_sys_version_id = 'https://github.com/duan-zehao/car_error_sys_version.git'
+server_sys_size_id = 'https://github.com/duan-zehao/car_error_sys_size.git'
+server_sys_code_id = 'https://github.com/duan-zehao/car_error_sys_code.git'
+temporary_server_sys_version_dir = '.'
+temporary_server_sys_version_name = '/temporary_version.txt'
+temporary_server_sys_size_dir = '.'
+temporary_server_sys_size_name = '/temporary_version.txt'
